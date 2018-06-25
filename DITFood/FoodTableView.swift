8fab04a6cb1fb76341f7f3d21cc40dd5e96a7f12
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-06-25 12:19:15 +0900

    add

---
+++ DITFood/FoodTableView.swift
@@ -0,0 +1,35 @@
+//
+//  FoodTableView.swift
+//  DITFood
+//
+//  Created by D7703_11 on 2018. 6. 25..
+//  Copyright © 2018년 dit.ac.kr. All rights reserved.
+//
+
+import UIKit
+
+class FoodTableView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

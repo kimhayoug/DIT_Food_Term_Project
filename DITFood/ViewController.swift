8f82bbcdcd5bca9eafa1a2e881215e4b8e4131b0
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-06-25 10:20:29 +0900

    Initial Commit

---
+++ DITFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DITFood
+//
+//  Created by D7703_11 on 2018. 6. 25..
+//  Copyright © 2018년 dit.ac.kr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
